23a317bffb034b121cac3aeee89cdb44e560201e
madguy <madguyinc@gmail.com>
2011-03-27 22:00:12 +0900

    教育解析、総コメント数プラグインを追加

---
+++ src/niconama_history_plugins/SearchEducation.py
@@ -0,0 +1,27 @@
+#-*- coding:utf-8
+
+from niconama_history.PluginBase import PluginBase
+import re
+
+class commentFilter(PluginBase):
+    """放送開始日を抽出するプラグインです。
+    """
+
+    def __init__(self, db):
+        PluginBase.__init__(self)
+
+    @property
+    def name(self):
+        return 'SeachEducation'
+
+    def analyzeDay(self, rows):
+        def replaceEducation(row):
+            name = row.name if row.name else '名無しさん'
+            match = re.search(u'教育\\s*?[\\(（](.+?)=(.+?)[\\）)]', row.message)
+            return u'{0}が 『{1}』 を 『{2}』 と教育しました。'.format(name, match.group(1), match.group(2))
+
+        educationRows = filter(lambda row: re.match(u'教育\\s*?[\\(（].+?=.+?[\\）)]', row.message), rows)
+        return map(replaceEducation ,educationRows)
+
+if __name__ == '__main__':
+    pass
---
+++ src/niconama_history_plugins/TotalComment.py
@@ -0,0 +1,25 @@
+#-*- coding:utf-8
+'''
+Created on 2011/03/20
+
+@author: madguy
+'''
+from niconama_history.PluginBase import PluginBase
+
+class commentFilter(PluginBase):
+    """コメントの統計を出力するプラグインです
+    """
+
+    def __init__(self, db):
+        PluginBase.__init__(self)
+        self.__flag = True
+
+    @property
+    def name(self):
+        return 'TotalComment'
+
+    def analyzeAll(self, rows):
+        return 'コメント数の総計は{0}件でした。'.format(len(rows))
+
+if __name__ == '__main__':
+    pass
